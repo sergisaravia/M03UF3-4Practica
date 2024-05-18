ff1f565694915019a9c2236cef4ba917fd48d630
sergisaravia <157466947+sergisaravia@users.noreply.github.com>
2024-05-18 13:08:49 +0200

    Add files via upload

--- src/Linea.java
+++ src/Linea.java
@@ -33,14 +33,84 @@ public class Linea implements Serializable {
     }
 
 
-    public static void EscribirLinea () {
-        ArrayList<String> arrayL1 = Estacion.leerVector();
-        String archivePath = "C:\\Users\\Sergi\\Downloads\\Red de metro\\src\\ArrayL1.txt";
+    public static void EscribirLineaL1() {
+        ArrayList<String> arrayL1 = Estacion.leerVectorL1(); //associació
+        String archivePath = "C:\\Users\\Sergi\\Downloads\\Red de metro\\src\\ArrayL1Resum.data";
         File file = new File(archivePath);
         try {
             PrintStream output = new PrintStream(file);
             for (int i = 0; i < arrayL1.size(); ++i) {
                 output.println(arrayL1.get(i));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static void EscribirLineaL2() {
+        ArrayList<String> arrayL2 = Estacion.leerVectorL2(); //associació
+        String archivePath = "C:\\Users\\Sergi\\Downloads\\Red de metro\\src\\ArrayL2Resum.data";
+        File file = new File(archivePath);
+        try {
+            PrintStream output = new PrintStream(file);
+            for (int i = 0; i < arrayL2.size(); ++i) {
+                output.println(arrayL2.get(i));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static void EscribirLineaL5() {
+        ArrayList<String> arrayL5 = Estacion.leerVectorL5(); //associació
+        String archivePath = "C:\\Users\\Sergi\\Downloads\\Red de metro\\src\\ArrayL5Resum.data";
+        File file = new File(archivePath);
+        try {
+            PrintStream output = new PrintStream(file);
+            for (int i = 0; i < arrayL5.size(); ++i) {
+                output.println(arrayL5.get(i));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static void EscribirLineaL9S() {
+        ArrayList<String> arrayL9S = Estacion.leerVectorL9S(); //associació
+        String archivePath = "C:\\Users\\Sergi\\Downloads\\Red de metro\\src\\ArrayL9SResum.data";
+        File file = new File(archivePath);
+        try {
+            PrintStream output = new PrintStream(file);
+            for (int i = 0; i < arrayL9S.size(); ++i) {
+                output.println(arrayL9S.get(i));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static void EscribirLineaL10S() {
+        ArrayList<String> arrayL10S = Estacion.leerVectorL10S(); //associació
+        String archivePath = "C:\\Users\\Sergi\\Downloads\\Red de metro\\src\\ArrayL10SResum.data";
+        File file = new File(archivePath);
+        try {
+            PrintStream output = new PrintStream(file);
+            for (int i = 0; i < arrayL10S.size(); ++i) {
+                output.println(arrayL10S.get(i));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static void EscribirLineaL12() {
+        ArrayList<String> arrayL12 = Estacion.leerVectorL12(); //associació
+        String archivePath = "C:\\Users\\Sergi\\Downloads\\Red de metro\\src\\ArrayL12Resum.data";
+        File file = new File(archivePath);
+        try {
+            PrintStream output = new PrintStream(file);
+            for (int i = 0; i < arrayL12.size(); ++i) {
+                output.println(arrayL12.get(i));
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        Estacion.escribirVector();
-        Estacion.leerVector();
-        Linea.EscribirLinea();
+        //Dependència
+        Estacion.escribirVectorL1();
+        Estacion.leerVectorL1();
+        Linea.EscribirLineaL1();
+        Estacion.escribirVectorL2();
+        Estacion.leerVectorL2();
+        Linea.EscribirLineaL2();
+        Estacion.escribirVectorL5();
+        Estacion.leerVectorL5();
+        Linea.EscribirLineaL5();
+        Estacion.escribirVectorL9S();
+        Estacion.leerVectorL9S();
+        Linea.EscribirLineaL9S();
+        Estacion.escribirVectorL10S();
+        Estacion.leerVectorL10S();
+        Linea.EscribirLineaL10S();
+        Estacion.escribirVectorL12();
+        Estacion.leerVectorL12();
+        Linea.EscribirLineaL12();
     }
 }
